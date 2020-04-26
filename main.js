853aa7e9c627d0dab1e7be83635eda66d91297b3
CodeKrakken <newholm1@hotmail.com>
2020-04-26 16:35:12 +0100

    Display tabs

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-Vue.component('root', {
+Vue.config.devtools = true
+
+Vue.component ('root', {
   template: `
-  <div>
-    Tricky Susan
-  </div>
+    <div>
+      Tricky Susan
+      <tabs></tabs>
+    </div>
   `
 })
+
+Vue.component ('tabs', {
+  template: `
+    <div>
+      <ul>
+        <span class="tab"
+        :class="{ activeTab: selectedTab === tab }"
+        v-for="(tab, index) in tabs"
+        @click="selectedTab = tab"
+        :key="index"
+        >{{ tab }}</span>
+      </ul>
+    <div>
+  `,
+  data() {
+    return {
+      tabs: ['News', 'Gigs', 'Shop', 'Bio'],
+      selectedTab: 'News'
+    }
+  }
+})
+
+Vue.component('news', {
+  template: `
+    <div>
+      Well ... nothing, obviously.
+    </div>
+  `
+})
+
 
 var app = new Vue({
   el: '#app'
