6fb8b43c3ca84024483830228a9132f136b4ee9a
CodeKrakken <newholm1@hotmail.com>
2020-04-28 19:07:38 +0100

    Add Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
