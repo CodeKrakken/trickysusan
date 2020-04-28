1094ae185741621e12f82abc9092205a267ab3d3
CodeKrakken <newholm1@hotmail.com>
2020-04-28 19:00:37 +0100

    Add express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import 'dotenv/config';
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(3000, () =>
+console.log('Example listening on port 3000!'),
+);
 console.log('Hello Endless Project.');
 console.log(process.env.MY_SECRET);
