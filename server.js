33574a7dfb24f15cde9c751fd770d8e750aed875
CodeKrakken <newholm1@hotmail.com>
2020-04-30 16:58:01 +0100

    Tidy file paths

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/frontend/public/index.html'))
+  res.sendFile(path.join(__dirname + '/frontend/index.html'))
 });
 
 app.listen(process.env.PORT, () =>
