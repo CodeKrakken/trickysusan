5157c79523b516d4765f536aceefdb9a370e801e
CodeKrakken <newholm1@hotmail.com>
2020-08-06 15:14:41 +0100

    Return routing to html style

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.php'))
+  res.sendFile(path.join(__dirname + '/index.html'))
 });
 
 app.listen((process.env.PORT || 3000), () =>
