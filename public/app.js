4eca9370e6bfac3f6fba50191ec46ac45be5f6d0
CodeKrakken <newholm1@hotmail.com>
2020-09-18 12:28:25 +0100

    Complete date formatting

--- public/app.js
+++ public/app.js
@@ -67,7 +67,7 @@ $(document).ready(function(){
   Vue.component('main-content', {
     template: `
       <div class="main-content">
-        <div v-show="selectedTab === 'News'"><nows /></div>
+        <div v-show="selectedTab === 'News'"><news /></div>
         <div v-show="selectedTab === 'Music'"><music /></div>
         <div v-show="selectedTab === 'Gigs'"><gigs /></div>
         <div v-show="selectedTab === 'Shop'"><shop /></div>
@@ -109,7 +109,7 @@ $(document).ready(function(){
   Vue.component('nows', {
     template: `
       <div>
-        {{ moment(posts[selectedPostIndex].date).format("Do MMM YYYY") }}
+        {{ moment(posts[selectedPostIndex].date).format('D.mM.YY') }}
         <br><br>
         {{ posts[selectedPostIndex].post }}
         <br><br>
