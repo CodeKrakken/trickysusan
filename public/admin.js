6870854458e788e2bcc429950fa7656e7fab7747
CodeKrakken <newholm1@hotmail.com>
2020-09-25 17:46:17 +0100

    Display news array in most basic way possible, but with a bit of style

--- public/admin.js
+++ public/admin.js
@@ -2,11 +2,40 @@ Vue.config.devtools = true
 
 $(document).ready(function(){
 
+  Vue.component ('background', {
+    template: `
+      <div>
+        <div id="background">
+      </div>
+    `
+  })
+
+  Vue.component('news', {
+    template: `
+      <div>
+        <div v-for="post in posts">
+          {{ post.date }} ... {{ post.post }}
+        </div>
+      </div>
+    `,
+    data() {
+      return {
+        posts: []
+      }
+    },
+    mounted() {
+      $.get("/news")
+      .then(response => (this.posts = response.reverse()))
+    }
+  })
+
   var admin = new Vue({
     el: '#admin',
     template: `
       <div>
-        Howdy! I am working just fine.
+        <background />
+        <band-name />
+        <news />
       </div>
     `  
   })
