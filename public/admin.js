ae5bfa0263390c29415f457a0afe7349a3146bc3
CodeKrakken <newholm1@hotmail.com>
2020-09-28 20:24:03 +0100

    Add login function. Not yet complete

--- public/admin.js
+++ public/admin.js
@@ -21,7 +21,16 @@ $(document).ready(function(){
     },
     methods: {
       login() {
-        loggedIn = true;
+        if (this.input.username && this.input.password) {
+          if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+            this.$emit("authenticated", true);
+            // this.$router.replace({ name: "secure" });
+          } else {
+            console.log("The username and / or password is incorrect");
+          }
+        } else {
+          console.log("A username and password must be present");
+        }
       }
     }
   })
