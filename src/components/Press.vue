e352a966992a1d99371512b101af33934977366c
CodeKrakken <newholm1@hotmail.com>
2021-05-01 19:54:27 +0100

    Add example code for button animation - not yet working

--- src/components/Press.vue
+++ src/components/Press.vue
@@ -93,6 +93,26 @@ export default {
   
 }
 
+@media (hover: none) {
+  @keyframes glowing {
+        0% {
+          background-color: #2ba805;
+          box-shadow: 0 0 5px #2ba805;
+        }
+        50% {
+          background-color: #49e819;
+          box-shadow: 0 0 20px #49e819;
+        }
+        100% {
+          background-color: #2ba805;
+          box-shadow: 0 0 5px #2ba805;
+        }
+      }
+      .image {
+        animation: glowing 1300ms infinite;
+      }
+}
+
 /* GENERAL */
 
 button {
