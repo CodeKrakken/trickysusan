daf1f6c5324997fd194dc7e7e1f51b1a1bc9bd09
CodeKrakken <newholm1@hotmail.com>
2021-05-01 16:59:01 +0100

    Move press image to center in portrait mode - but text display is off

--- src/components/Press.vue
+++ src/components/Press.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="press">
+  <div id="videos">
     <div id="navigator">
       <button @click="previous()" class="nav-button">
         <img src="/images/previous-button.png" class="icon" />
@@ -8,14 +8,14 @@
         <img src="/images/next-button.png" class="icon" />
       </button>
     </div>
-    <div id="release-row">
+    <div id="video-outer">
       <button id="link" @click="visit(press[selectedPressIndex].link)">
         <img class="image" :src="press[selectedPressIndex].image" />
       </button>
       <br><br>
       <div v-html="press[selectedPressIndex].description" />
     </div>
-  </div>  
+  </div>
 </template>
 
 <script>
@@ -34,7 +34,34 @@ export default {
           description: 'Interview<br>Unrated Magazine<br>26.02.20'
         }
       ],
-      selectedPressIndex: this.getIndex() || 0
+      selectedPressIndex: this.getIndex() || 0,
+      videos: [
+        {
+          name: 'Grab',
+          address: "https://www.youtube.com/embed/9_Tnuuq7exI?rel=0&modestbranding=1"
+        },
+        {
+          name: 'Undercover',
+          address: "https://www.youtube.com/embed/ZZUYN2O9bdU?rel=0&modestbranding=1"
+        },
+        {
+          name: "Strategy",
+          address: "https://www.youtube.com/embed/exggrODRu4w?rel=0&modestbranding=1"
+        },
+        {
+          name: "Tug of War",
+          address: "https://www.youtube.com/embed/hm2jNxkG2ak?rel=0&modestbranding=1&start=4"
+        },
+        {
+          name: "Come On Over (session)",
+          address: "https://www.youtube.com/embed/X3lryMPttIY?rel=0&modestbranding=1"
+        },
+        {
+          name: "Baby, I Do",
+          address: "https://www.youtube.com/embed/qTJmtTHc31U?rel=0&modestbranding=1"
+        }
+      ],
+      selectedVideoIndex: (this.getIndex() || 0)
     }
   },
   methods: {
@@ -54,7 +81,7 @@ export default {
       sessionStorage.selectedPressIndex = this.selectedPressIndex
     },
     getIndex() {
-      return parseInt(sessionStorage.selectedPressIndex, 10)
+      return parseInt(sessionStorage.selectedPressIndex)
     },
     visit(link) {
       window.open(link, '_blank');
@@ -65,136 +92,108 @@ export default {
 
 <style scoped>
 
-  @media screen and ( orientation: landscape ) {
+/* PORTRAIT VERSION */
 
-    #press {
-      width: 69vw;
-      height: 50vh;
-      font-size:calc(6px + 1.5vw);
-    }
-
-    #release-row {
-      text-align: center
-      /* display: grid; */
-    }
-
-    #navigator {
-      top: calc(10px + 2vh);
-      margin: auto auto
-    }
-
-    .nav-button {
-      width: 3vw;
-      min-width: 40px;
-    }
-
-  }
-
-  @media screen and ( orientation: portrait ) {
-
-    #press {
-      width: 90vw;
-      height: 80vh;
-      font-size:calc(10px + 1.5vh);
-    }
-
-    #release-row {
-      display: flex;
-      flex-direction: column;
-      text-align: center
-    }
-
-    #navigator {
-      top: 10px;
-      margin: auto auto
-    }
-
-    .nav-button {
-      height: 4vh;
-    }
-  }
-
-  #press {
-    display: flex;
-    flex-direction: column;
-    align-items: center
-  }
-
-  .nav-button:focus {
-    filter: brightness(50%);
-    outline: none;
-  }
-
-  .nav-button {
-    filter: brightness(50%);
-    background: black;
-    transition: all 0.1s ease;
-    border: none;
-    min-width: 40px;
-    width: 3vw;
-    box-shadow: 3px 3px 3px black, 
-              3px -3px 3px black,
-              -3px -3px 3px black,
-              -3px 3px 3px black;
-  }
-
-  .nav-button:hover {
-    filter: brightness(75%);
-    transition: all 0.2s ease;
-  }
-
-  .nav-button:active {
-    filter: brightness(100%);
-    transition: all 0.1s ease;
+@media screen and (orientation: portrait) {
+  iframe {
+    height: 32vh; 
+    width: 100vw;
   }
 
   #navigator {
-    display: flex;
+    top: 10px;
+    margin: auto auto
+  }
+}
+
+/* LANDSCAPE VERSION */
+
+@media screen and (orientation: landscape) {
+
+  #navigator {
+    top: calc(10px + 2vh);
+    margin: auto auto
+  }
+
+  #video-outer {
     position: absolute;
-    justify-content: center;
-    background: transparent;
-    align-content: center;
-    border: none;
+    top: 12vh;
+    bottom: 10px;
+    width: 69vw
   }
 
-  .icon { width: 100% }
-
-  #release-header {
-    font-size: calc(42px + 1.5vw)
+  iframe {
+    width: 100%;
+    height: 100%
   }
 
-  #release-details {
-    font-size: calc(20px);
-    font-style: italic
-  }
+}
 
-  .text {
-    text-align: justify
-  }
+/* GENERAL */
 
-  .image {
-    height: 30vh;
-    box-shadow:  10px  10px 10px black, 
-               10px -10px 10px black, 
-              -10px -10px 10px black, 
-              -10px  10px 10px black
-  }
+.image {
+  height: 30vh;
+  box-shadow:  10px  10px 10px black, 
+              10px -10px 10px black, 
+            -10px -10px 10px black, 
+            -10px  10px 10px black
+}
 
-  a {
-    text-decoration: none
-  }
+#link:hover {
+  filter: invert();
+  transition: all 0.2s ease;
+}
 
-  a:link {
-    color: #00aeef
-  }
+#videos {
+  display: flex;
+  flex-direction: column;
+  align-items: center
+}
 
-  a:visited {
-    color: #E22076
-  }
+#video-outer {
+  box-shadow:  5px  5px 5px black,
+               5px -5px 5px black, 
+              -5px -5px 5px black, 
+              -5px  5px 5px black;
+}
 
-  #link:hover {
-    filter: invert();
-    transition: all 0.2s ease;
-  }
-  
+#navigator {
+  display: flex;
+  position: absolute;
+  justify-content: center;
+  background: transparent;
+  align-content: center;
+  border: none
+}
+
+.nav-button:focus {
+  filter: brightness(50%);
+  outline: none;
+}
+
+.nav-button {
+  filter: brightness(50%);
+  background: black;
+  transition: all 0.1s ease;
+  border: none;
+  min-width: 40px;
+  width: 3vw;
+  box-shadow: 3px 3px 3px black, 
+            3px -3px 3px black,
+            -3px -3px 3px black,
+            -3px 3px 3px black;
+}
+
+.nav-button:hover {
+  filter: brightness(75%);
+  transition: all 0.2s ease;
+}
+
+.nav-button:active {
+  filter: brightness(100%);
+  transition: all 0.1s ease;
+}
+
+.icon { width: 100% }
+
 </style>
